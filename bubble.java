fba593097174ec8d1fe27d56d9b1445daeaccad9
Nishant Kumar <nishantdinayak11@gmail.com>
2021-10-28 01:11:22 +0530

    add bubbleSort

---
+++ bubble.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class bubble{
+    public static void main(String[] args) {
+        int arr[] ={5,2,9,4,8,0,1};
+        int temp = 0;
+        System.out.println(arr.length);
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-1-i; j++){
+                if(arr[j] > arr[j+1]){
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                
+            }
+        }
+        for(int i=0;i<arr.length; i++){
+         System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
